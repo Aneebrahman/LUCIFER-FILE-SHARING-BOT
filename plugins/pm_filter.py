0b4ba70fc55504913de279735fe198bd6c831f24
Aneebrahman <108203396+Aneebrahman@users.noreply.github.com>
2022-12-23 20:27:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -134,7 +134,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"✪ [{get_size(file.file_size)}✪ {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"✪ {get_size(file.file_size)} ✪ {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -146,7 +146,7 @@ async def next_page(bot, query):
                     text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
                 InlineKeyboardButton(
-                    text=f"✪ {get_size(file.file_size)}✪",
+                    text=f"✪ {get_size(file.file_size)} ✪",
                     callback_data=f'files_#{file.file_id}',
                 ),
             ]
@@ -1043,7 +1043,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"✪ [{get_size(file.file_size)}✪ {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"✪ {get_size(file.file_size)} ✪ {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -1056,7 +1056,7 @@ async def auto_filter(client, msg, spoll=False):
                     callback_data=f'{pre}#{file.file_id}',
                 ),
                 InlineKeyboardButton(
-                    text=f"✪ {get_size(file.file_size)}✪",
+                    text=f"✪ {get_size(file.file_size)} ✪",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
             ]
