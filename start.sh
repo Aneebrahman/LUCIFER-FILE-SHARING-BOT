abebddaacead3409b740cf0697237c11c099c961
Aneebrahman <108203396+Aneebrahman@users.noreply.github.com>
2023-02-05 21:06:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/CrazyDeveloperTG/Doctor-Strange.git /Doctor-Strange
+  git clone https://github.com/Aneebrahman/LUCIFER-FILE-SHARING-BOT.git /LUCIFER FILE SHARE BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Doctor-Strange
+  git clone $UPSTREAM_REPO /LUCIFER FILE SHARE BOT
 fi
-cd /Doctor-Strange
+cd /LUCIFER FILE SHARE BOT
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜....🍁"
 python3 bot.py
